a34cc7b78e2da908165c5f6eccdfa28a529056f3
alexander.bau <alexander.bau@nfon.com>
2021-09-05 03:20:33 +0200

    added docs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CurrentlyShownComponent } from './components/currently-shown/currently-shown.component';
 import { ItemNamePipe } from './pipes/item-name.pipe';
+import { environment } from 'src/environments/environment';
+import { APP_BASE_HREF } from '@angular/common';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatListModule } from '@angular/material/list';
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule( {
   declarations: [
@@ -29,7 +31,7 @@ import {MatSelectModule} from '@angular/material/select';
     MatListModule,
     MatSelectModule
   ],
-  providers: [],
+  providers: [ { provide: APP_BASE_HREF, useValue: environment.APP_BASE_HREF } ],
   bootstrap: [ AppComponent ]
 } )
 export class AppModule { }
